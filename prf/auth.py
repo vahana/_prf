52749206443079a86220f4f5b90129a4fae636c8
Vahan <vayvazyan@butterflynetinc.com>
2015-01-04 15:34:31 -0500

    * moved AccountView into auth

--- prf/auth.py
+++ prf/auth.py
@@ -23,7 +23,6 @@ def enable_auth(config, user_model=None, root_factory=RootACL,
 
     user_model = config.maybe_dotted(user_model)
 
-    from prf.utility_views import AccountView
     AccountView.set_user_model(user_model)
 
     authn_policy = AuthTktAuthenticationPolicy(secret,
@@ -36,12 +35,61 @@ def enable_auth(config, user_model=None, root_factory=RootACL,
     config.registry._auth = True
 
     config.add_route('prf_login', '%s/%s' % (route_prefix, login_path))
-    config.add_view(view='prf.utility_views.AccountView', attr='login',
+    config.add_view(view=AccountView, attr='login',
                     route_name='prf_login', request_method='POST')
 
     config.add_route('prf_logout', '%s/%s' % (route_prefix, logout_path))
-    config.add_view(view='prf.utility_views.AccountView', attr='logout',
+    config.add_view(view=AccountView, attr='logout',
                     route_name='prf_logout')
 
 def includeme(config):
     config.add_directive('enable_auth', enable_auth)
+
+
+class AccountView(object):
+
+    __user_model = None
+
+    def __init__(self, request):
+        self.request = request
+
+    @classmethod
+    def set_user_model(cls, model):
+
+        def check_callable(model, name):
+            if not getattr(model, name, None):
+                raise AttributeError("%s model must have '%s' class method"
+                                     % (model, name))
+
+        check_callable(model, 'groupfinder')
+        check_callable(model, 'authenticate')
+
+        cls.__user_model = model
+
+    @classmethod
+    def groupfinder(cls, userid, request):
+        return cls.__user_model.groupfinder(userid, request)
+
+    def login(self):
+        login = self.request.params['login']
+        password = self.request.params['password']
+        next = self.request.params.get('next', '')
+
+        success, user = self.__user_model.authenticate(login, password)
+        if success:
+            headers = remember(self.request, user)
+            if next :
+                return JHTTPFound(headers=headers, location=next)
+            return JHTTPOk(headers=headers)
+        else:
+            raise JHTTPUnauthorized("User '%s' failed to Login" % login)
+
+    def logout(self):
+        next = self.request.params.get('next', '')
+
+        headers = forget(self.request)
+
+        if next:
+            return JHTTPFound(headers=headers, location=next)
+
+        return JHTTPOk(headers=headers)
