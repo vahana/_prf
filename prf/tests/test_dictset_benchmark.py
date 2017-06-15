23794390373cd8fb4709e3256365e797dcbe1ab3
Cyril <cyril@hippie.io>
2017-06-15 14:39:35 -0400

    Cleanup benchmarks

--- prf/tests/test_dictset_benchmark.py
+++ prf/tests/test_dictset_benchmark.py
@@ -19,6 +19,7 @@ def options(group):
 
 
 LOREM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut dictum nibh, non congue dolor. Nulla sollicitudin nunc ac nisl vestibulum auctor. Sed molestie dignissim feugiat. Etiam placerat justo arcu, et euismod sapien varius eu. Maecenas pellentesque, sapien non sagittis dapibus, tellus odio porttitor est, sit amet congue lorem nisl in nulla. Nullam rhoncus nisl tellus, eu sodales ligula eleifend quis. Cras accumsan in purus sit amet efficitur. Mauris et felis varius, mollis massa vel, vestibulum turpis. Vivamus euismod libero a lacus ultricies, quis convallis mauris tempus. Vivamus commodo gravida hendrerit.'
+# LOREM = 'L'
 
 
 class TestDictSetBenchmark(object):
@@ -29,8 +30,8 @@ class TestDictSetBenchmark(object):
         d={
             'i': [LOREM] * 10,
             'ii': {
-                '1.': LOREM,
-                '2.': [5, 6, 7],
+                'aa': LOREM,
+                'ab': [5, 6, 7],
             }
         },
         e={
@@ -63,22 +64,21 @@ class TestDictSetBenchmark(object):
     @pytest.mark.benchmark(**options('extract'))
     def test_extract(self, benchmark):
         d = dictset(self.sample_d)
-        r = benchmark(d.extract, ['a', 'b'])
+        benchmark(d.extract, ['a', 'b', 'c'])
 
     @pytest.mark.benchmark(**options('extract'))
-    def test_extract_invalid_key(self, benchmark):
+    def test_extract_nested(self, benchmark):
         d = dictset(self.sample_d)
-        # d.ii is valid, it's a very slow lookup, and it doesn't return
-        r = benchmark(d.extract, ['a', 'b', 'd.ii'])
+        benchmark(d.extract, ['a', 'b', 'c', 'd.ii.*'])
 
     @pytest.mark.benchmark(**options('subset'))
     def test_subset(self, benchmark):
         d = dictset(self.sample_d)
-        r = benchmark(d.subset, ['a', 'b', 'd'])
+        benchmark(d.subset, ['a', 'b', 'd'])
 
     @pytest.mark.benchmark(**options('update_with'))
     def test_update_with(self, benchmark):
         # Include d in both to have a collision
         d = dictset(self.sample_d).subset(['a', 'b', 'd'])
         e = dictset(self.sample_d).subset(['c', 'd', 'e'])
-        r = benchmark(d.update_with, e)
+        benchmark(d.update_with, e)
