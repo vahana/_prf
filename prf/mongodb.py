74cf7e68f96ea9c63890d51c19ab3771c4abacf0
vahan <vahan@gazelle.company>
2017-07-11 23:41:57 +0000

    * removed misleading comment

--- prf/mongodb.py
+++ prf/mongodb.py
@@ -35,7 +35,6 @@ class TopLevelDocumentMetaclass(TLDMetaclass):
 
 
 def get_document_cls(name, _raise=True):
-    # Avoid using this method, prefer `prf.dataset.get_document`
     try:
         return mongo.document.get_document(name)
     except Exception as e:
